d85e60d4d9b8655fc2a42506bdff245234fbc4dc
Alvin <alvin.cl.ng@gmail.com>
2017-02-16 11:15:01 +0000

    Add conditionals exercises

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
-function sayHello(name) {
-  console.log("Hello, " + name)
+function ageCalculator (name, yearOfBirth, currentYear){
+  var age = currentYear - yearOfBirth;
+  return name + " is " + age + " years old";
 }
 
-sayHello("Caliban")
-sayHello("Miranda")
-sayHello("Ferdinand")
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
